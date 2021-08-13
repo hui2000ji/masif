1301d0656f5d2c87682b16004cdd7cdb0d1940fb
hui2000ji <391085296@qq.com>
2021-08-13 23:28:19 +0800

    bug fix 4

--- data/masif_ligand/data_prepare_one.sh
+++ data/masif_ligand/data_prepare_one.sh
@@ -11,5 +11,5 @@ module load reduce/20180820
 # ~/anaconda3/envs/py2/bin/python $masif_source/data_preparation/00b-generate_assembly.py $1
 # ~/anaconda3/envs/py2/bin/python $masif_source/data_preparation/00c-save_ligand_coords.py $1
 conda activate my-rdkit-env
-~/anaconda3/envs/my-rdkit-env/bin/python data_preparation.01-pdb_extract_and_triangulate $1 masif_ligand
-~/anaconda3/envs/my-rdkit-env/bin/python data_preparation.04c-make_protein_pyg $1
+~/anaconda3/envs/my-rdkit-env/bin/python -m data_preparation.01-pdb_extract_and_triangulate $1 masif_ligand
+~/anaconda3/envs/my-rdkit-env/bin/python -m data_preparation.04c-make_protein_pyg $1
