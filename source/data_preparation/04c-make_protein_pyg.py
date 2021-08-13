2749ecd2841e6d1513938204219d5d93279a0b0b
hui2000ji <391085296@qq.com>
2021-08-13 22:41:19 +0800

    bug fix

--- source/data_preparation/04c-make_protein_pyg.py
+++ source/data_preparation/04c-make_protein_pyg.py
@@ -9,11 +9,11 @@ from default_config.masif_opts import masif_opts
 from masif_modules.read_data_from_surface import read_data_from_surface
 
 
-def make_protein_pyg(d, save_dir=Path(masif_opts['ligand']['masif_pyg_dir']), exists_ok=True):
-    if (save_dir / f'{d.name}.pt').exists() and exists_ok:
+def make_protein_pyg(prot_id, save_dir=Path(masif_opts['ligand']['masif_pyg_dir']), exists_ok=True):
+    if (save_dir / f'{prot_id}.pt').exists() and exists_ok:
         return
     # Read directly from the ply file.
-    ply_file = masif_opts['ply_file_template'].format(d.name)
+    ply_file = masif_opts['ply_file_template'].format(prot_id)
         
     # Compute shape complementarity between the two proteins. 
     input_feat, rho, theta, mask, list_indices, _, pos, normal = read_data_from_surface(ply_file, masif_opts['ligand'])
@@ -32,16 +32,16 @@ def make_protein_pyg(d, save_dir=Path(masif_opts['ligand']['masif_pyg_dir']), ex
     theta = theta[mask]
     edge_attr = np.stack([rho, theta, ddc_src, ddc_tgt], axis=1)
 
-    tdata = Data(x=torch.FloatTensor(x), edge_index=torch.LongTensor(edge_index), edge_attr=torch.FloatTensor(edge_attr), normal=torch.FloatTensor(normal), pos=torch.FloatTensor(pos), id=d.name)
-    torch.save(tdata, save_dir / f'{d.name}.pt')
+    tdata = Data(x=torch.FloatTensor(x), edge_index=torch.LongTensor(edge_index), edge_attr=torch.FloatTensor(edge_attr), normal=torch.FloatTensor(normal), pos=torch.FloatTensor(pos), id=prot_id)
+    torch.save(tdata, save_dir / f'{prot_id}.pt')
 
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        root = Path(masif_opts['ligand']['masif_precomputation_dir'])
-        dirs = list(root.iterdir())
+        root = Path(masif_opts['ligand']['ply_chain_dir'])
+        files = root.glob('*.ply')
+        stems = [f.stem for f in files]
         with Pool(4) as p:
-            p.map(make_protein_pyg, dirs)
+            p.map(make_protein_pyg, stems)
     else:
-        d = sys.argv[1]
-        make_protein_pyg(d)
+        make_protein_pyg(sys.argv[1])
